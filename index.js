b0420a3c66e87b539ed8810d9ebabb40d071e80f
Byron Wang <byronwang123919@gmail.com>
2022-12-05 19:54:24 -0800

    finished

---
+++ index.js
@@ -0,0 +1,9 @@
+const jokeButton = document.getElementById("joke-btn");
+
+jokeButton.addEventListener("click", function () {
+  alert("Justin Beaver!");
+});
+
+// jokeButton.onclick = function () {
+//   alert("Justin Beaver! :)");
+// };
